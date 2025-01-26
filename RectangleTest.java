590715ffd11bb116beb86d6e36c56d09f171e53e
Raphaelexander <benalexander2020@gmail.com>
2025-01-26 11:19:28 -0800

    RectangleTest.java
    Added Kennedi's test class

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,4 +1,17 @@
 
 class RectangleTest{
-
+public static void testRectangle(int x1, int y1, int x4, int y4) { // tests rectangle
+    
+}
+    
+    private static int calculateArea(int x1, int y1, int x4, int y4) { // calculates the area of the rectangle
+       int length = Math.abs(x4 - x1);
+       int width = Math.abs(y4 - y1);
+       return length * width;
+    }
+    private static int calculatePerimeter(int x1, int y1, int x4, int y4) { // calculates the perimeter of the rectangle
+        int length = Math.abs(x4 - x1);
+        int width = Math.abs(y4 - y1);
+        return 2 * (length + width);
+    }   
 }
