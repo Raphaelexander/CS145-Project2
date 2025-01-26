2c31700b5bdec5b937928cec037559060f7a1827
Raphaelexander <benalexander2020@gmail.com>
2025-01-26 15:07:53 -0800

    RectangleTest.java
    Working version of RectangleTest

--- RectangleTest.java
+++ RectangleTest.java
@@ -1,17 +1,30 @@
+class RectangleTest {
 
-class RectangleTest{
-public static void testRectangle(int x1, int y1, int x4, int y4) { // tests rectangle
-    
-}
-    
-    private static int calculateArea(int x1, int y1, int x4, int y4) { // calculates the area of the rectangle
-       int length = Math.abs(x4 - x1);
-       int width = Math.abs(y4 - y1);
-       return length * width;
+    public static void findPoints(int x1, int y1, int x4, int y4) {
+        int x2 = x1;
+        int y3 = y1;
+        int y2 = y4;
+        int x3 = x1;
+
+        System.out.println("Point x1 is located at (" + x1 + ", " + y1 + ")");
+        System.out.println("Point x2 is located at (" + x2 + ", " + y2 + ")");
+        System.out.println("Point x3 is located at (" + x3 + ", " + y3 + ")");
+        System.out.println("Point x4 is located at (" + x4 + ", " + y4 + ")");
     }
-    private static int calculatePerimeter(int x1, int y1, int x4, int y4) { // calculates the perimeter of the rectangle
+
+    public static void testRectangle(int x1, int y1, int x4, int y4) { // tests rectangle
+
+    }
+
+    public static int calculateArea(int x1, int y1, int x4, int y4) { // calculates the area of the rectangle
+        int length = Math.abs(x4 - x1);
+        int width = Math.abs(y4 - y1);
+        return length * width;
+    }
+
+    public static int calculatePerimeter(int x1, int y1, int x4, int y4) { // calculates the perimeter of the rectangle
         int length = Math.abs(x4 - x1);
         int width = Math.abs(y4 - y1);
         return 2 * (length + width);
-    }   
+    }
 }
