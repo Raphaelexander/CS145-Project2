959e2f521f0848591786f21bdf3210deab11303b
Raphaelexander <benalexander2020@gmail.com>
2025-01-26 15:07:14 -0800

    Rectangle.java
    Working version of Rectangle.

--- Rectangle.java
+++ Rectangle.java
@@ -1,25 +1,29 @@
 import java.util.Scanner;
 
-public class Rectangle{
+public class Rectangle {
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Input the x1 coordinate:");
         System.out.print("x2--x4\n-    -\n-    -\nx1--x3");
         int x1 = input.nextInt();
         int y1 = input.nextInt();
-        
-        if(x1 < 0 || y1 < 0) {
-            System.out.print("Invalid input, please input a positive integer");
-        } else {
-            System.out.println("\nInput the x4 coordinate:");
-            int x4 = input.nextInt();
-            int y4 = input.nextInt();
 
-            if(x4 < 0 || y4 < 0) {
-                System.out.print("Invalid input, please input a positive integer");
-            }
+        if (x1 < 0 || y1 < 0) {
+            System.out.print("Invalid input, please input a positive integer");
         }
 
-        //RectangleTest.newRectangle = RectangleTest.function(int x1, int y1, int x4, int y4);
+        System.out.println("\nInput the x4 coordinate:");
+        int x4 = input.nextInt();
+        int y4 = input.nextInt();
+
+        if (x4 < 0 || y4 < 0) {
+            System.out.print("Invalid input, please input a positive integer");
+        }
+
+        //RectangleTest rectangle = new RectangleTest();
+        RectangleTest.findPoints(x1, y1, x4, y4);
+        System.out.println("The area is " + RectangleTest.calculateArea(x1, y1, x4, y4));
+        System.out.print("The perimeter is " + RectangleTest.calculatePerimeter(x1, y1, x4, y4));
     }
 }
