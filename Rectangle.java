45b4c5f4bb9004fdccc664fd7a6affbf6c3d3028
raphaelexander <benalexander2020@gmail.com>
2025-01-28 21:46:24 -0800

    Final rectangle with comments

--- Rectangle.java
+++ Rectangle.java
@@ -5,20 +5,20 @@ public class Rectangle {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Input the x1 coordinate:");
-        System.out.print("x2--x4\n-    -\n-    -\nx1--x3");
+        System.out.print("x2--x4\n-    -\n-    -\nx1--x3"); //prints a rectangle to the user to describe which points go where
         int x1 = input.nextInt();
         int y1 = input.nextInt();
         
-        RectangleTest.testPointOne(x1, y1);
+        RectangleTest.testPointOne(x1, y1); //tests if point1 of shape is between 0-20
 
         System.out.println("\nInput the x4 coordinate:");
         int x4 = input.nextInt();
         int y4 = input.nextInt();
 
-        RectangleTest.testPointFour(x4, y4);
+        RectangleTest.testPointFour(x4, y4); //tests if point4 of shape is between 0-20
 
-        RectangleTest.findPoints(x1, y1, x4, y4);
-        System.out.println("The area is " + RectangleTest.calculateArea(x1, y1, x4, y4));
-        System.out.print("The perimeter is " + RectangleTest.calculatePerimeter(x1, y1, x4, y4));
+        RectangleTest.findPoints(x1, y1, x4, y4); //final output to user
+        System.out.println("The area is " + RectangleTest.calculateArea(x1, y1, x4, y4)); //final output to user
+        System.out.print("The perimeter is " + RectangleTest.calculatePerimeter(x1, y1, x4, y4)); //final output to user
     }
 }
--- RectangleTest.java
+++ RectangleTest.java
@@ -1,36 +1,40 @@
 class RectangleTest {
 
-    public static void findPoints(int x1, int y1, int x4, int y4) {
+    public static void findPoints(int x1, int y1, int x4, int y4) { //takes user input and finds other points to make rectangle or square
         int x2 = x1;
         int y3 = y1;
         int y2 = y4;
-        int x3 = x1;
+        int x3 = x4;
 
-        System.out.println("Point x1 is located at (" + x1 + ", " + y1 + ")");
+        System.out.println("Point x1 is located at (" + x1 + ", " + y1 + ")"); //prints out to user
         System.out.println("Point x2 is located at (" + x2 + ", " + y2 + ")");
         System.out.println("Point x3 is located at (" + x3 + ", " + y3 + ")");
         System.out.println("Point x4 is located at (" + x4 + ", " + y4 + ")");
     }
 
-    public static void testPointOne(int x1, int y1) {
-         if (x1 < 0 || y1 < 0) {
-            System.out.print("Invalid input, please input a positive integer");
+    public static void testPointOne(int x1, int y1) { //tests user points
+         if (x1 < 0 || y1 < 0 || x1 > 20 || y1 > 20) {
+            System.out.print("Invalid input, please input an integer between 0 - 20");
+            System.exit(0);
         }
     }
 
-    public static void testPointFour(int x4, int y4) {
-        if (x4 < 0 || y4 < 0) {
-            System.out.print("Invalid input, please input a positive integer");
+    public static void testPointFour(int x4, int y4) { //tests user points
+        if (x4 < 0 || y4 < 0 || x4 > 20 || y4 > 20) {
+            System.out.print("Invalid input, please input an integer between 0 - 20");
+            System.exit(0);
         }
-    }
-
-    public static void testRectangle(int x1, int y1, int x4, int y4) { // tests rectangle
-
     }
 
     public static int calculateArea(int x1, int y1, int x4, int y4) { // calculates the area of the rectangle
         int length = Math.abs(x4 - x1);
         int width = Math.abs(y4 - y1);
+
+        if (length == width) {
+            System.out.println("This shape is a square.");
+        } else {
+            System.out.println("This shape is a rectanlge.");
+        }
         return length * width;
     }
 
